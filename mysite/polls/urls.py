0ab42e9416e087a1381e13862bad5e5ec2fd60c1
Ola <austineolanrewaju@gmail.com>
2021-04-18 18:27:12 +0100

    Using Generic View

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     # for polls.views.index 
-    path('', views.index, name="index"),
+    path('', views.IndexView.as_view(), name="index"),
     # for polls.views.details which is polls/question_id 
-    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     # for polls.views.results which is polls/question_id/results 
-    path('<int:question_id>/results/', views.results, name="results"),
+    path('<int:pk>/results/', views.ResultView.as_view(), name="results"),
     # for polls.views.vote which is polls/qiestion_id/vote
     path('<int:question_id>/vote/', views.vote, name ="vote"),
     ]
