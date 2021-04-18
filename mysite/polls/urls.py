dfa67133099b5ecf0c7bed2cd212c0a75d200e47
Ola <austineolanrewaju@gmail.com>
2021-04-18 15:13:48 +0100

    Trying to use the question models to design a view before the use of templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    
-    
+    # for polls.views.index 
+    path('', views.index, name="index"),
+    # for polls.views.details which is polls/question_id 
+    path('<int:question_id>/', views.detail, name="details"),
+    # for polls.views.results which is polls/question_id/results 
+    path('<int:question_id>/results/', views.results, name="results"),
+    # for polls.views.vote which is polls/qiestion_id/vote
+    path('<int:question_id>/vote/', views.vote, name ="vote"),
     ]
